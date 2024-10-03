55d808feaa579a595a99287cf74a12f025f6f288
dat truong <thelegendaglx@gmail.com>
2024-10-03 19:01:16 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FormsModule,
+    InputTextModule,
+    DialogModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tailwind-primeng-template';
+  userName: string = '';
+  visible = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 }
